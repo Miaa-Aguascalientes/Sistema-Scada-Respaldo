27087f32727740772d69115364a51d68fede5df0
Miaa <pedroguillermomiaa@gmail.com>
2026-05-05 14:15:34 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -1940,14 +1940,32 @@ if sectores_data:
                 </div>
             """
             
-            # --- RENDERIZADO EN MAPA (Se mantiene igual) ---
-            folium.Marker(location=info['coord'], icon=folium.DivIcon(html=f'<div style="font-size: 9px; font-weight: bold; color: {info["color_final"]};">{id_p}</div>')).add_to(m)
-            
-            pop = folium.Popup(html_popup, max_width=450)
+            folium.Marker(
+                location=info['coord'],
+                icon=folium.DivIcon(
+                    icon_size=(150,36),
+                    icon_anchor=(-12, 10),
+                    html=f'<div style="font-size: 9px; font-weight: bold; color: {info["color_final"]}; white-space: nowrap; text-shadow: 1px 1px #000; pointer-events: none;">{id_p}</div>'
+                )
+            ).add_to(m)
+
             if info.get('blink'):
-                folium.Marker(location=info['coord'], icon=folium.DivIcon(html=get_blink_icon(info['color_final'])), popup=pop).add_to(m)
+                folium.Marker(
+                    location=info['coord'],
+                    icon=folium.DivIcon(html=get_blink_icon(info['color_final'])),
+                    popup=folium.Popup(html_popup, max_width=450)
+                ).add_to(m)
             else:
-                folium.CircleMarker(location=info['coord'], radius=4, color=info['color_final'], fill=True, fill_color=info['color_final'], popup=pop).add_to(m)
+                folium.CircleMarker(
+                    location=info['coord'],
+                    radius=4,
+                    color=info['color_final'],
+                    fill=True,
+                    fill_color=info['color_final'],
+                    fill_opacity=1,
+                    popup=folium.Popup(html_popup, max_width=450)
+                ).add_to(m)
+
 
 # 9.7. RENDERIZADO DE TANQUES EN EL MAPA PRINCIPAL ---------------------------------------------------------------------------------------
     if ver_tanques:
