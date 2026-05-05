4c13b24bbddf8ddbb9ba970dd0ae6d986764f437
Miaa <pedroguillermomiaa@gmail.com>
2026-05-05 11:31:19 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -1988,4 +1988,4 @@ if sectores_data:
                 MousePosition().add_to(m_sec)
            
     folium.LayerControl(position='topright', collapsed=False).add_to(m)          
-    folium_static(m, width=None, height=500)
+    folium_static(m, width=500, height=700)
