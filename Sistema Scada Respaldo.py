d56793788058276932d974a3d2d1f5afbea7a6de
Miaa <pedroguillermomiaa@gmail.com>
2026-05-06 10:29:04 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -1952,6 +1952,7 @@ for id_p, info in mapa_pozos_dict.items():
             ).add_to(m)
 
 
+            st_folium(m, width="100%", height=700)
 
 # 9.7. RENDERIZADO DE TANQUES EN EL MAPA PRINCIPAL ---------------------------------------------------------------------------------------
     if ver_tanques:
