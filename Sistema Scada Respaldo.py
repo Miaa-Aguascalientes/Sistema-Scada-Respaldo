b7398dc4bae89b9837e15acbbfc122e28a9dd706
Miaa <pedroguillermomiaa@gmail.com>
2026-05-13 09:24:56 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -1423,9 +1423,7 @@ if sector_seleccionado:
                                 fill='tozeroy',
                                 fillcolor='rgba(0, 212, 255, 0.05)', # Color del área (con transparencia opcional)
                                 yaxis="y2",
-                                line=dict(color='#ff00ff', width=2),
-                                dash='dash',
-                                ),
+                                line=dict(color='#ff00ff', width=2,dash='dash'),
                                 hovertemplate='Presión P1: %{y:.2f} kg/cm2<extra></extra>'
                             ))
 
