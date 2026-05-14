918c8fda18205e3da7207d05ed12510265a561fd
Miaa <pedroguillermomiaa@gmail.com>
2026-05-14 10:40:12 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -1635,7 +1635,7 @@ if sector_seleccionado:
                             legend=dict(
                                 orientation="h",
                                 yanchor="bottom",
-                                y=-1.20,
+                                y=-0.0,
                                 x=0.5,
                                 xanchor="center",
                                 font=dict(color="white", size=9)
