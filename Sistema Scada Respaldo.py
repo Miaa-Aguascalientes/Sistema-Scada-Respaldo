5c661d9dca4d4c5098306518618da9b9e0835b91
Miaa <pedroguillermomiaa@gmail.com>
2026-05-15 07:48:16 -0600

    Reduce figure height from 380 to 300

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -1740,7 +1740,7 @@ if sector_seleccionado:
                         fig_v.update_layout(
                             paper_bgcolor='rgba(0,0,0,0)', 
                             plot_bgcolor='rgba(0,0,0,0)', 
-                            height=380, 
+                            height=300, 
                             margin=dict(l=50, r=50, t=10, b=10), 
                             hovermode="x unified", 
                             # Mantenemos la leyenda abajo para que no estorbe
