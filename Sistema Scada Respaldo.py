f83d51671aa38964e665b4787410946ff543bdd4
Miaa <pedroguillermomiaa@gmail.com>
2026-05-18 09:01:54 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -2000,7 +2000,7 @@ with st.sidebar:
     with st.expander("🗺️ Control de Capas", expanded=False):
         ver_sectores = st.checkbox("Mostrar Sectores", value=True)
         ver_pozos = st.checkbox("Mostrar Pozos", value=True)
-        ver_tanques = st.checkbox("Mostrar Tanques", value=False)
+        ver_tanques = st.checkbox("Mostrar Tanques", value=True)
         ver_rebombeos = st.checkbox("Mostrar Rebombeos", value=False) # Activado por defecto para facilitar localización
     
     # 8.9. LISTADO DE ESTADOS ---
