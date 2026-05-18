4cc2c39bbf815dd0c9473047c5a995ea29d616b1
Miaa <pedroguillermomiaa@gmail.com>
2026-05-18 10:42:50 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -123,7 +123,7 @@ if not st.session_state.autenticado:
         <div class="visual-core">
             <div class="ring r1"></div><div class="ring r2"></div>
             <div class="center-logo">
-                <img src="https://raw.githubusercontent.com/Miaa-Aguascalientes/Sistema-Scada/b501401ee7b1a2428dbb38f08887517b058e544e/LogoMIAA.svg?token=BLAUM5JZJYJQLBLEKUVVFEDKBMUVU" class="logo-miaa">
+                <img src="https://raw.githubusercontent.com/Miaa-Aguascalientes/Sistema-Scada/6e854dd38ce32e1e9dd83ffe2acc399985582d06/LogoMIAA.svg?token=BLAUM5IOP7BT2N6O6K3C6HLKBNAT6" class="logo-miaa">
                 <h2 style="color:#00d4ff; font-family:Orbitron; font-size:-400px; letter-spacing:5px; margin-top:-35px;"></h2>
             </div>
         </div>
@@ -1921,7 +1921,7 @@ st.markdown("""
 
 with st.sidebar:
     # 8.1. Contenedor del logo
-    st.markdown('<div class="sidebar-logo"><img src="https://raw.githubusercontent.com/Miaa-Aguascalientes/Sistema-Scada/b501401ee7b1a2428dbb38f08887517b058e544e/LogoMIAA.svg?token=BLAUM5JZJYJQLBLEKUVVFEDKBMUVU"></div>', unsafe_allow_html=True)
+    st.markdown('<div class="sidebar-logo"><img src="https://raw.githubusercontent.com/Miaa-Aguascalientes/Sistema-Scada/6e854dd38ce32e1e9dd83ffe2acc399985582d06/LogoMIAA.svg?token=BLAUM5IOP7BT2N6O6K3C6HLKBNAT6"></div>', unsafe_allow_html=True)
 
     # 8.2. Inicializamos variables de estado (Solo si no existen)
     if 'centro_mapa' not in st.session_state:
