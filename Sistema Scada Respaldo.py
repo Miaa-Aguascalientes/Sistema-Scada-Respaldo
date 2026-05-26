0eb3ed9708a5b6f45f468d8d9a318ac346d42252
Miaa <pedroguillermomiaa@gmail.com>
2026-05-26 12:49:54 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -1135,7 +1135,7 @@ if "ver_grafico" in st.query_params:
     """, unsafe_allow_html=True)
 
     # --- Selector de fechas ---
-    col_sel, col_ind, col_btn = st.columns([1.0, 0.5, 6.0])
+    col_sel, col_ind, col_btn = st.columns([2.0, 0.5, 6.0])
 
     with col_sel:
         st.markdown('<div style="margin-top: 26px;"></div>', unsafe_allow_html=True)
