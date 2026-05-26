32f2d0107268976839eae5b73a435379b2f324cf
Miaa <pedroguillermomiaa@gmail.com>
2026-05-26 12:59:31 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -1116,10 +1116,13 @@ if "ver_grafico" in st.query_params:
         <style>
             @keyframes spin {{ from {{ transform: rotate(0deg); }} to {{ transform: rotate(360deg); }} }}
             .spin-icon {{ animation: spin 4s linear infinite; display: inline-block; vertical-align: middle; margin-right: 15px; }}
+            .logo-miaa {{ height: 35px; margin-right: 15px; vertical-align: middle; }}
             div[data-testid="column"] {{ padding-top: 0px !important; }}
             div[data-testid="stVerticalBlock"] {{ gap: 0px !important; }}
         </style>
         <div style="display: flex; align-items: center; background-color: #0e1117; padding: 10px 20px; border-radius: 10px; border: 1px solid #30363d; margin-bottom: 10px;">
+            <img src="https://raw.githubusercontent.com/Miaa-Aguascalientes/Logos/38504978c8f77a4dac38ad476f74dbdee6af2cad/LogoMIAA.svg" class="logo-miaa">
+            
             <svg class="spin-icon" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#00FFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="12" cy="12" r="10"></circle>
                 <path d="M12 2v4M12 18v4M4.93 4.93l2.83 2.83M16.24 16.24l2.83 2.83M2 12h4M18 12h4M4.93 19.07l2.83-2.83M16.24 7.76l2.83-2.83"></path>
