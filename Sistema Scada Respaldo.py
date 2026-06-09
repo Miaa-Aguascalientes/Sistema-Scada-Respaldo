e566acbcbbf771481256421d6ee0ccdd369eedbd
Miaa <pedroguillermomiaa@gmail.com>
2026-06-09 14:57:22 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -2382,8 +2382,8 @@ if sector_seleccionado:
                                     # La opacidad se mantiene en 0.15 (15%)
                                     fillcolor=color_base.replace("hsl", "hsla").replace(")", ", 0.15)"),
                                     
-                                    hovertemplate='<b>%{x|%d-%b-%Y %H:%M}</b><br>' +
-                                                  '<b>%{fullData.name}</b>: %{y:.2f} ' + unidad_pc + '<extra></extra>'
+                                    hovertemplate='<b>%{fullData.name}</b>: %{y:.2f} ' + unidad_pc + '<extra></extra>'
+                                                 
                                 ))
 
                         delta = pd.Timedelta(hours=1)
@@ -2417,7 +2417,7 @@ if sector_seleccionado:
                             yaxis=dict(
                                 title="Caudales (m³/h)", 
                                 color="#00d4ff", 
-                                tickformat=""
+                                tickformat="%d-%b-%Y %H:%M:%S",
                             ),
                             yaxis2=dict(
                                 title="Presiones (kg)", 
@@ -2425,7 +2425,7 @@ if sector_seleccionado:
                                 overlaying="y", 
                                 color="#00ff00", 
                                 showgrid=False, 
-                                tickformat=""
+                                tickformat="%d-%b-%Y %H:%M:%S",
                             )
                         )
                         
