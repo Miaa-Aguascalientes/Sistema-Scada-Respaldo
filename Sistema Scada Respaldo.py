50a476a68c93e58601f1925e65804a9b4df4f5fa
Miaa <pedroguillermomiaa@gmail.com>
2026-06-09 10:07:38 -0600

    Update Sistema Scada Respaldo.py

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -632,13 +632,15 @@ if tag_a_graficar:
 
             # 1. Definimos las fechas de tus líneas y el diccionario de traducción
             dias_es = {0: 'Lun', 1: 'Mar', 2: 'Mié', 3: 'Jue', 4: 'Vie', 5: 'Sáb', 6: 'Dom'}
+            meses_es = {1: 'Ene', 2: 'Feb', 3: 'Mar', 4: 'Abr', 5: 'May', 6: 'Jun', 
+                        7: 'Jul', 8: 'Ago', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dic'}
             
             # Generamos las posiciones exactas de tus líneas (inicio de cada día)
             fechas_lineas = pd.date_range(start=fecha_inicio, end=fecha_fin, freq='D')
             
             # 2. Construimos etiquetas explícitas: Hora <br> Día Español DD-MM-AAAA
             etiquetas_finales = [
-                f"{d.strftime('%H:%M')}<br>{dias_es[d.dayofweek]} {d.strftime('%d-%m-%Y')}"
+                f"{d.strftime('%H:%M')}<br>{dias_es[d.dayofweek]} {d.strftime('%d-%b-%Y')}"
                 for d in fechas_lineas
             ]
 
