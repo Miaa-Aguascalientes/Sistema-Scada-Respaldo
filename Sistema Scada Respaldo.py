2ef33bdbcbd09559c8875dc9b89caf6d631520f9
Miaa <pedroguillermomiaa@gmail.com>
2026-08-10 12:23:02 -0600

    Remove unnecessary line in folium LayerControl

--- Sistema Scada Respaldo.py
+++ Sistema Scada Respaldo.py
@@ -3747,7 +3747,7 @@ if ver_colonias:
 
 
     # 9.11. CONTROL DE CAPAS Y RENDERIZADO FINAL 
-    folium.LayerControl(position='topright', collapsed=False).add_to(m)
+    
     folium_static(m, width=None, height=600)
 
     # --- % DE AFECTACIONES POR COLONIAS ---
